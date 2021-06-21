843b86af6f010b06e84ba4494ab378c249ebdc9f
Angel Soto <85462911+TEODOE-LC101@users.noreply.github.com>
2021-06-21 23:23:44 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+//loop through the array and print each value
+
+let array = ["St. Louis", "Philly", "Kansas City", "Northwest Arkansas"];
+
+//Print every third number, 1-100
+
+
+
+//while loop -- print even numbers 0-25
+
+
+
+//reverse string
+
+let string = "hello";
